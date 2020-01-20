c227780a8d3a5c6769c3f3d4c9c2168a615d07ea
Richard_yyf <501975087@qq.com>
2020-01-20 15:51:45 +0800

    submit demo code for【SpringBoot 微服务应用集成Prometheus + Grafana实现监控告警】

---
+++ src/main/java/com/ricstudio/springbootactuatorprometheustest/config/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.ricstudio.springbootactuatorprometheustest.config;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Richard_yyf
+ * @version 1.0 2020/1/19
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @Resource
+    private PrometheusCustomMonitor monitor;
+
+    @ResponseBody
+    @ExceptionHandler(value = Exception.class)
+    public String handle(Exception e) {
+        monitor.getRequestErrorCount().increment();
+        return "error, message: " + e.getMessage();
+    }
+}
--- src/main/java/com/ricstudio/springbootactuatorprometheustest/controller/TestController.java
+++ src/main/java/com/ricstudio/springbootactuatorprometheustest/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.ricstudio.springbootactuatorprometheustest.controller;
 
+import com.ricstudio.springbootactuatorprometheustest.config.PrometheusCustomMonitor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -12,6 +16,9 @@ import java.util.concurrent.TimeUnit;
  */
 @RestController
 public class TestController {
+
+    @Resource
+    private PrometheusCustomMonitor monitor;
 
     @GetMapping("/hello")
     public String hello() {
@@ -24,5 +31,17 @@ public class TestController {
         int delay = random.nextInt(10);
         TimeUnit.SECONDS.sleep(delay);
         return "sleep time: " + delay;
+    }
+
+    @RequestMapping("/order")
+    public String order(@RequestParam(defaultValue = "0") String flag) throws Exception {
+        monitor.getOrderCount().increment();
+        if ("1".equals(flag)) {
+            throw new Exception("出错啦");
+        }
+        Random random = new Random();
+        int amount = random.nextInt(100);
+        monitor.getAmountSum().record(amount);
+        return "下单成功, 金额: " + amount;
     }
 }
